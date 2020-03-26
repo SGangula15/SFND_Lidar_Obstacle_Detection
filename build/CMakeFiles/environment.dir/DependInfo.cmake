8523b3631416f953919b47da1c359808ea81e0bc
SGangula15 <gradaute.srikanth@gmail.com>
2020-03-26 01:01:08 -0400

    mac os setup check- not working

---
+++ build/CMakeFiles/environment.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/srikanthgangula/Documents/GitHub/SFND_Lidar_Obstacle_Detection/src/environment.cpp" "/Users/srikanthgangula/Documents/GitHub/SFND_Lidar_Obstacle_Detection/build/CMakeFiles/environment.dir/src/environment.cpp.o"
+  "/Users/srikanthgangula/Documents/GitHub/SFND_Lidar_Obstacle_Detection/src/processPointClouds.cpp" "/Users/srikanthgangula/Documents/GitHub/SFND_Lidar_Obstacle_Detection/build/CMakeFiles/environment.dir/src/processPointClouds.cpp.o"
+  "/Users/srikanthgangula/Documents/GitHub/SFND_Lidar_Obstacle_Detection/src/render/render.cpp" "/Users/srikanthgangula/Documents/GitHub/SFND_Lidar_Obstacle_Detection/build/CMakeFiles/environment.dir/src/render/render.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_OPENNI"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "vtkDomainsChemistry_AUTOINIT=1(vtkDomainsChemistryOpenGL2)"
+  "vtkIOExport_AUTOINIT=2(vtkIOExportOpenGL2,vtkIOExportPDF)"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  "vtkRenderingFreeType_AUTOINIT=1(vtkRenderingFreeTypeFontConfig)"
+  "vtkRenderingOpenGL2_AUTOINIT=1(vtkRenderingGL2PSOpenGL2)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/vtk/8.2.0_8/include/vtk-8.2"
+  "/usr/local/opt/python/Frameworks/Python.framework/Versions/3.7/include/python3.7m"
+  "/usr/local/include"
+  "/usr/local/opt/hdf5/include"
+  "/usr/local/opt/szip/include"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/usr/include/libxml2"
+  "/usr/local/include/pcl-1.6"
+  "/opt/local/include/eigen3"
+  "/opt/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/processPointClouds.cpp
+++ src/processPointClouds.cpp
@@ -53,8 +53,25 @@ std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT
 {
     // Time segmentation process
     auto startTime = std::chrono::steady_clock::now();
-	pcl::PointIndices::Ptr inliers;
+
     // TODO:: Fill in this function to find inliers for the cloud.
+    pcl::SACSegmentation<PointT> seg;
+    pcl::PointIndices::Ptr inliers {new pcl::PointIndices};
+    pcl::ModelCoefficients::Ptr coefficients {new pcl::ModelCoefficients};
+
+    seg.setOptimizeCoefficients(true);
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setMaxIterations(maxIterations);
+    seg.setDistanceThreshold(distanceThreshold);
+
+    //Segment the largest planar component from the input point cloud
+    seg.setInputCloud(cloud);
+    seg.segment(*inliers, *coefficients);
+    if(inliers->indices.size() == 0)
+    {
+        std::cout << "Cloud not estimate a planar model for the given point cloud dataset" << std::endl;
+    }
 
     auto endTime = std::chrono::steady_clock::now();
     auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
